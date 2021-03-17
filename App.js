f65e7398a7884da8bc5754bc0ad0d564b69a3b69
budbud-io <lejoshua298@gmail.com>
2021-03-17 12:44:03 -0500

    Input fields added

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+
   return (
-    <View style={styles.container}>,
-      <Text>Hello Klein Carpool</Text>,
-      <StatusBar style="auto" />
+    <View style={styles.container}>
+
+      <Text>Enter name:</Text>
+      <TextInput 
+        placeholder='e.g. Joshua Le' 
+        style={styles.input}
+        onChangeText={(value) => setName(value)} />
+
+      <Text>Enter age:</Text>
+      <TextInput 
+        placeholder='e.g. 17' 
+        style={styles.input}
+        onChangeText={(value) => setAge(value)} />
+
+      <Text style={styles.result}>Name: {name}, Age: {age}</Text>
+  
     </View>
   );
 }
@@ -14,8 +29,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#48E5C2',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderWidth: 1.5,
+    borderColor: '#3690a5',
+    padding: 8,
+    margin: 10,
+    width: 200,
+  }
 });
+
