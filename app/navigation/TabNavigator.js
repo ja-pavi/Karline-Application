09c95ad8da5d704f235461db57428aed22f90a40
Jacob Villanueva <jacobpaulvillanueva@gmail.com>
2021-03-18 22:35:21 -0500

    Menu Icons and Color

--- app/navigation/TabNavigator.js
+++ app/navigation/TabNavigator.js
@@ -3,10 +3,40 @@ import{createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import Home from '../components/Home'
 import Profile from '../components/Profile'
 import Create from '../components/Create'
+import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator()
 const TabNavigator = () => {
- return <Tab.Navigator>
+ return <Tab.Navigator
+ screenOptions={({ route }) => ({
+     tabBarIcon: ({
+        focused, color, size }) => {
+
+            let iconName;
+
+            if (route.name === "I'm Here") {
+            iconName = focused
+            ? 'car'
+            : 'car-outline';} 
+            else if (route.name === 'Settings') {
+            iconName = focused
+            ? 'cog'
+            : 'cog-outline';}
+            else if (route.name === 'Student Info') {
+            iconName = focused
+            ? 'person-circle'
+            : 'person-circle-outline';}
+            
+            return <Ionicons name={iconName} size={size} color={color}     />;
+            },
+            })}
+        
+            tabBarOptions={{
+            activeTintColor: '#577399',
+            inactiveTintColor: 'gray',
+            }}
+           >
+        
      <Tab.Screen name="I'm Here" component={Home} />
      <Tab.Screen name="Student Info" component={Profile} />
      <Tab.Screen name="Settings" component={Create} />
