6bd94fec784ce16f6615dfdcac4b782ec4afd39b
Jacob Villanueva <jacobpaulvillanueva@gmail.com>
2021-03-21 18:00:22 -0500

    linear gradient, beginning

--- app/navigation/StackNavigator.js
+++ app/navigation/StackNavigator.js
@@ -3,9 +3,9 @@ import{createStackNavigator} from '@react-navigation/stack';
 import SignIn from '../screens/SignIn';
 import SignUp from '../screens/SignUp';
 import Splash from '../screens/Splash';
-import { BlurView } from 'expo';
 
 const Stack = createStackNavigator();
+
 const StackNavigator = () => {
 return (
 <Stack.Navigator
--- app/screens/Splash.js
+++ app/screens/Splash.js
@@ -1,39 +1,84 @@
 import React from 'react'
-import { View, StyleSheet, Text, Button } from 'react-native'
+import { View, StyleSheet, Text, TouchableOpacity, Dimensions, Image} from 'react-native'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import {LinearGradient} from 'expo-linear-gradient';
 
-const Splash = ({navigation}) => {
- return(
-  <View style={styles.container}>
-    <Text style={styles.text}>Splash Screen</Text>
-    
-    <Button 
-      title="Click to Sign In"
-      color="#577399"
-      onPress={() => navigation.navigate("Sign In")}
-      />
 
-      <Button 
-      title="Click to Sign Up"
-      color="#577399"
-      onPress={() => navigation.navigate("Sign Up")}
-      />
+const Splash = () => {
+  return(
 
-  </View>
-  )
-}
+    <View style={styles.container}>
+      <View style={styles.header}>
+      </View>
+      <View style={styles.footer}> 
+        <Text style={styles.title}>Stay connected with everyone!</Text>
+        <Text style={styles.text}>Sign in with account</Text>
+        <TouchableOpacity onPress={alert('Click')}>
+          <LinearGradient
+            colors={['#08d4c4', '#01ab9d']}
+            style={styles.signIn}
+            >
+              <Text style={styles.textSign}>Get Started</Text>
+            </LinearGradient>
+        </TouchableOpacity>
 
-const styles = StyleSheet.create({
+
+      </View>
+    </View>
+   );
+ };
+
+const {height} = Dimensions.get("screen");
+const height_logo = height * 0.28;
+
+
+ const styles = StyleSheet.create({
   container: {
-   flex: 1,
-   justifyContent: 'center',
-   alignItems: 'center',
-   backgroundColor: "#F0EFEB"
+    flex: 1, 
+    backgroundColor: '#009387'
   },
-
-  text: {
+  header: {
+      flex: 2,
+      justifyContent: 'center',
+      alignItems: 'center'
+  },
+  footer: {
+      flex: 1,
+      backgroundColor: '#fff',
+      borderTopLeftRadius: 30,
+      borderTopRightRadius: 30,
+      paddingVertical: 50,
+      paddingHorizontal: 30
+  },
+  logo: {
+      width: height_logo,
+      height: height_logo
+  },
+  title: {
+      color: '#05375a',
       fontSize: 30,
-      fontWeight: 'bold',
-      color: "#DCD9D0"
+      fontWeight: 'bold'
+  },
+  text: {
+      color: 'grey',
+      marginTop:5
+  },
+  button: {
+      alignItems: 'flex-end',
+      marginTop: 30
+  },
+  signIn: {
+      width: 150,
+      height: 40,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 50,
+      flexDirection: 'row'
+  },
+  textSign: {
+      color: 'white',
+      fontWeight: 'bold'
   }
-})
-export default Splash; 
+});
+
+export default Splash;                          
