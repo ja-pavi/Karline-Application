b19601a86dbaaa5656537ff62672e28f38e00025
Jacob Villanueva <jacobpaulvillanueva@gmail.com>
2021-03-21 21:42:38 -0500

    UI Sign In and Logo

--- app/screens/Splash.js
+++ app/screens/Splash.js
@@ -1,30 +1,46 @@
 import React from 'react'
 import { View, StyleSheet, Text, TouchableOpacity, Dimensions, Image} from 'react-native'
+import * as Animatable from 'react-native-animatable';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 import {LinearGradient} from 'expo-linear-gradient';
-
+import { useTheme } from '@react-navigation/native';
 
 const Splash = () => {
   return(
-
     <View style={styles.container}>
-      <View style={styles.header}>
+    <StatusBar backgroundColor='#009387' barStyle="light-content"/>
+    <View style={styles.header}>    
+    <Animatable.Image 
+      animation="bounceIn"
+      duraton="1500"
+      source={require("../../assets/splashlogo.png")}
+      style={styles.logo}
+      resizeMode="stretch"
+      />
       </View>
-      <View style={styles.footer}> 
-        <Text style={styles.title}>Stay connected with everyone!</Text>
-        <Text style={styles.text}>Sign in with account</Text>
-        <TouchableOpacity onPress={alert('Click')}>
-          <LinearGradient
-            colors={['#08d4c4', '#01ab9d']}
-            style={styles.signIn}
-            >
-              <Text style={styles.textSign}>Get Started</Text>
-            </LinearGradient>
-        </TouchableOpacity>
-
-
+      <Animatable.View
+      animation="bounceIn"
+      duraton="1500" 
+      style={styles.footer}> 
+          <Text style={styles.title}>The new way to carpool!</Text>
+          <Text style={styles.text}>Sign in with account</Text>
+          <View style={styles.button}>
+      <TouchableOpacity onPress={alert('Click')}>
+      <LinearGradient
+          colors={['#5DB6CA', '#97D0DD']}
+          style={styles.signIn}
+        >
+        <Text style={styles.textSign}>Get Started</Text>
+        <MaterialIcons
+          name="navigate-next"
+          color="#fff"
+          size={20}
+          />
+        </LinearGradient>
+      </TouchableOpacity>
       </View>
-    </View>
+      </Animatable.View>
+      </View>
    );
  };
 
@@ -35,7 +51,7 @@ const height_logo = height * 0.28;
  const styles = StyleSheet.create({
   container: {
     flex: 1, 
-    backgroundColor: '#009387'
+    backgroundColor: '#5DB6CA'
   },
   header: {
       flex: 2,
@@ -51,8 +67,8 @@ const height_logo = height * 0.28;
       paddingHorizontal: 30
   },
   logo: {
-      width: height_logo,
-      height: height_logo
+      width: 400,
+      height: 400
   },
   title: {
       color: '#05375a',
@@ -65,7 +81,8 @@ const height_logo = height * 0.28;
   },
   button: {
       alignItems: 'flex-end',
-      marginTop: 30
+      marginTop: 30,
+      
   },
   signIn: {
       width: 150,
