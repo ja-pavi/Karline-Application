4968d7fea4d03748bd16bdc62afb734d41a9137f
Jacob Villanueva <jacobpaulvillanueva@gmail.com>
2021-04-07 14:25:14 -0500

    lit

---
+++ app/components/AddCar.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { View, StyleSheet, Animated, Image} from 'react-native'
+import {LinearGradient} from 'expo-linear-gradient'
+import * as Animatable from 'react-native-animatable';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+
+
+const AddCar = () => {
+  return(
+  <View style={styles.container}>
+    <LinearGradient
+     colors={['#6DD5FA', '#2993b9', '#FFFFFF']}
+     start={{ x: 0, y: 0 }}
+     end={{ x: 1 , y: 1 }}
+     style={styles.background}/> 
+  </View>
+   )
+ }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignContent: 'center'
+  },
+  header: {
+    flex: 2,
+    justifyContent: 'center',
+    marginTop: 80,
+    flexDirection: 'row',
+},
+  background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: 1000,
+  },
+  text: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: "#6dd5ed"
+  },
+  logo: { 
+    width: 30,
+    height: 30,
+    marginHorizontal: 0,
+  },
+  smoke: { 
+    marginTop: 14,
+    width: 10,
+    height: 10,
+    marginRight: 14,
+  }
+})
+export default AddCar;                          
--- app/navigation/TabNavigator.js
+++ app/navigation/TabNavigator.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import{createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import Here from '../components/Here'
+import AddCar from '../components/AddCar'
 import Settings from '../components/Settings'
 import { Ionicons } from '@expo/vector-icons';
 import AddStudent from '../components/AddStudent';
@@ -17,8 +18,8 @@ const TabNavigator = () => {
 
             if (route.name === "I'm Here") {
             iconName = focused
-            ? 'car'
-            : 'car-outline';} 
+            ? 'checkmark-circle'
+            : 'checkmark-circle-outline';} 
             else if (route.name === 'Settings') {
             iconName = focused
             ? 'options'
@@ -27,6 +28,11 @@ const TabNavigator = () => {
             iconName = focused
             ? 'person'
             : 'person-outline';}
+            else if (route.name === 'Add Car') {
+            iconName = focused
+            ? 'car'
+            : 'car-outline'
+            }
             
             return <Ionicons name={iconName} size={size} color={color}     />;
             },
@@ -37,9 +43,10 @@ const TabNavigator = () => {
             inactiveTintColor: 'gray',
             }}
            >
-        
+     
      <Tab.Screen name="I'm Here" component={Here} />
      <Tab.Screen name="Student Info" component={AddStudent} />
+     <Tab.Screen name="Add Car" component={AddCar} />
      <Tab.Screen name="Settings" component={Settings} />
  </Tab.Navigator>
 }
